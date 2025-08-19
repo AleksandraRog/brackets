7c969c8de92fbf44c48e706accea53831fc8295a
Aleksandra <170029753+AleksandraRog@users.noreply.github.com>
2025-08-19 06:57:23 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  let nStr = str;
+  const regExpStr = bracketsConfig
+    .map((val) =>
+      val.map((value) => value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('')
+    )
+    .join('|');
+
+  const regExp = new RegExp(regExpStr, 'g');
+  while (regExp.test(nStr)) {
+    nStr = nStr.replace(regExp, '');
+  }
+  return nStr.length === 0;
 };
